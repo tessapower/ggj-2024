57c847717532d4bc2903cf482cd279feb8e5f85b
Adam Goodyear <therealzedxc@gmail.com>
2024-01-22 18:37:53 +1300

    Fonts and Juggling
    Fonts added and Juggling minigame base created

---
+++ jest-quest/scripts/managers/JugglingMinigame.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var clickable = false
+var score = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if clickable:
+		if Input.is_action_just_pressed("LeftClick"):
+			score += 1
+			print(score)
+			
+func toggleClickable():
+	clickable = !clickable
