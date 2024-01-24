203bbb06e194b7a7c43360db3198e58fec773de6
Tessa Power <hello@tessapower.xyz>
2024-01-24 12:33:21 -0600

    Create game over screen with placeholder graphics

---
+++ jest-to-impress/scripts/ui/game_over.gd
@@ -0,0 +1,23 @@
+extends Node
+
+# game_over.gd: This script displays the player's final score on game over and
+#               responding to the player clicking any of the navigation buttons.
+#
+# Author(s): Tessa Power
+
+@onready var final_score_label = get_node("Content/FinalScore")
+
+func _ready() -> void:
+	final_score_label.text += "\n" + str(GamestateManager.score + 529)
+
+
+func _on_play_again_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_return_to_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/ui/start_menu.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
