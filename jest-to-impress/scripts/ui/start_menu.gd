e070af777f669c7c00b351c5542d607e0827b928
Tessa Power <hello@tessapower.xyz>
2024-01-23 19:40:45 -0600

    Add submenus into start menu

---
+++ jest-to-impress/scripts/ui/start_menu.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+# start_menu.gd: This script handles responding to button being clicked and
+#                submenus opening/closing on the start menu.
+#
+# Author(s): Tessa Power
+
+@onready var buttons : Array = $Content/Buttons.get_children()
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_submenu_opened() -> void:
+	for button in buttons:
+		button.disabled = true
+
+
+func _on_submenu_closed() -> void:
+	for button in buttons:
+		button.disabled = false
