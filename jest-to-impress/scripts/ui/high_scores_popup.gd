820f5bd4d5242196cbd3649c25bb6c51a320f90d
Tessa Power <hello@tessapower.xyz>
2024-01-26 13:56:52 -0600

    Fix bug with high scores

--- jest-to-impress/scripts/ui/high_scores_popup.gd
+++ jest-to-impress/scripts/ui/high_scores_popup.gd
@@ -5,12 +5,15 @@ extends Node
 #
 # Author(s): Tessa Power
 
+const EMPTY_TEXT : String = "[center][b]High Scores[/b][/center]
+[center]There are no high scores yet.[/center]"
+
 func _ready():
 	var list = scores_to_list(HighScoresManager.high_scores)
-	$Submenu/Content/TextContent.set_text(list)
+	$Submenu/Content/ScrollContainer/TextContent.set_text(list)
 
 func scores_to_list(scores : Array) -> String:
-	if scores.is_empty(): return "There are no high scores."
+	if scores.is_empty(): return EMPTY_TEXT
 
 	var list = ""
 	for score in scores:
