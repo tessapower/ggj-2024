9d6d0ff44a7e76f81f933676d4a0118d7ea4c0ab
Tessa Power <hello@tessapower.xyz>
2024-01-26 17:44:19 -0600

    Refactor Gamestate Manager to reduce attention meter code

--- jest-to-impress/scripts/managers/gamestate_manager.gd
+++ jest-to-impress/scripts/managers/gamestate_manager.gd
@@ -9,43 +9,40 @@ extends Node2D
 #
 # Author(s): Adam Goodyear, Tessa Power
 
-# Player stats
+# Player Score
 var score : int = 0
+const SCORE_INCREASE : int = 1
 var score_multipier : int = 1
-var show_tutorials : bool = true
 
 # Game state
 var speed : float = 1.0
 const speed_change : float = 0.5
 var current_round : int = 1
-
-var attention = Timer.new()
-const ATTENTION_METER_MAX : float = 30
-
-enum attention_levels {
-	BAD = 0,
-	OK = 1,
-	GOOD = 2,
-	GREAT = 3
-}
-
 var is_paused : bool = false
+var show_tutorials : bool = true
+
+# Attention Meter
+var attention_meter = Timer.new()
+const ATTENTION_METER_MAX : float = 30
+enum AttentionChange {BAD = -5, OK = 5, GOOD = 7, GREAT = 10}
 
 func _ready():
-	attention.wait_time = ATTENTION_METER_MAX
-	attention.one_shot = true
-	add_child(attention)
+	attention_meter.wait_time = ATTENTION_METER_MAX
+	attention_meter.one_shot = true
+	add_child(attention_meter)
 	reset()
 
+
 func reset() -> void:
-	attention.start(ATTENTION_METER_MAX)
+	attention_meter.start(ATTENTION_METER_MAX)
 	score = 0
 	speed = 1.0
 	current_round = 1
 	score_multipier = 1
 
-func increase_score(points_won : int) -> void:
-	score += points_won * score_multipier
+
+func increase_score() -> void:
+	score += SCORE_INCREASE * score_multipier
 
 
 func next_round() -> void:
@@ -70,37 +67,30 @@ func resume() -> void:
 	get_tree().paused = false
 	#SoundManager.resume_music()
 
-# Increases the time left on the attention meter
-func increase_attention_meter(attention_change : int) -> void:
-	if attention.time_left + attention_change > ATTENTION_METER_MAX:
-		attention.start(ATTENTION_METER_MAX)
-		score_multipier += 1
-	else:
-		attention.start(attention.time_left + attention_change)
-		score_multipier = 1
 
-# Decreases the time left on the attention meter
-func decrease_attention_meter(attention_change : int) -> void:
-	if attention.time_left - attention_change < 0:
+# Updates the attention meter given the player's performance in a mini-game.
+func update_attention_meter(rating : MiniGame.Rating):
+	# Calculate the change based on the given rating
+	var change = 0
+	match rating:
+		MiniGame.Rating.FAILED: change  = AttentionChange.BAD
+		MiniGame.Rating.AVERAGE: change = AttentionChange.OK
+		MiniGame.Rating.GOOD: change    = AttentionChange.GOOD
+		MiniGame.Rating.PERFECT: change = AttentionChange.GREAT
+
+	# Add the change to the time left on the attention meter
+	var new_val = attention_meter.time_left + change
+
+	# Respond to the change appropriately, ending the game if attention is zero
+	if new_val > ATTENTION_METER_MAX:
+		attention_meter.start(ATTENTION_METER_MAX)
+		score_multipier += 1
+	elif new_val <= 0:
 		end_game()
 	else:
-		attention.start(attention.time_left - attention_change)
+		attention_meter.start(new_val)
 		score_multipier = 1
 
-# Sets the attention meters values
-func set_attention_meter(attention_change : int) -> void:
-	attention.start(attention_change)
-
-func calculate_attention(arg1 : attention_levels):
-	match arg1:
-		attention_levels.BAD:
-			decrease_attention_meter(5)
-		attention_levels.OK:
-			increase_attention_meter(5)
-		attention_levels.GOOD:
-			increase_attention_meter(7)
-		attention_levels.GREAT:
-			increase_attention_meter(10)
 
 func end_game():
 	get_tree().change_scene_to_file("res://scenes/ui/game_over.tscn")
