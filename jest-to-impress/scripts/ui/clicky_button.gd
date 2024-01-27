eacaa09a646c8a3de294293d8dd761824ae9dd03
Tessa Power <hello@tessapower.xyz>
2024-01-27 12:19:57 -0600

    Remove unused clicky button script

--- jest-to-impress/scripts/ui/clicky_button.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-@onready var click_sound: AudioStream = load("res://assets/audio/sfx/Button_press.mp3")
-
-func _on_pressed() -> void:
-	if click_sound:
-		SoundManager.play_ui_sound(click_sound)
