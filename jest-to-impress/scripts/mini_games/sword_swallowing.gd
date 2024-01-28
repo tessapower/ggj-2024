e9468512cbad56aaa79e469a0a183930413c6bdc
Tessa Power <hello@tessapower.xyz>
2024-01-27 20:41:23 -0600

    Add missing has_played bool to sword swallowing game

--- jest-to-impress/scripts/mini_games/sword_swallowing.gd
+++ jest-to-impress/scripts/mini_games/sword_swallowing.gd
@@ -3,6 +3,8 @@ extends "res://scripts/mini_games/mini_game.gd"
 var sword_offset
 var sword_hovered = false
 
+static var has_played : bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Tutorial.connect("close_requested", self.hide_tutorial)
